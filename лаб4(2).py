3f591d3600a6b968cdb62ab304a5bbaed54cd6d7
Veronika Bilousova <113579879+VeronikaBilousova@users.noreply.github.com>
2022-09-20 13:08:41 +0300

    Add files via upload

---
+++ лаб4(2).py
@@ -0,0 +1,7 @@
+import math
+x=int(input("Введіть значення x: "))
+a=int(input("Введіть значення а: "))
+b=int(input("Введіть значення b: "))
+c=int(input("Введіть значення c: "))
+Y=float(pow(x,2)+math.log(a,math.e)-math.sin(b)*math.sqrt(math.fabs(c)))
+print(Y)
